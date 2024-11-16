6447e1304c1a295c3b90179487623d848e44beb0
Nugine <nugine@foxmail.com>
2024-11-16 19:03:57 +0800

    feat: add mod macros, ptr, default

---
+++ src/default.rs
@@ -0,0 +1,10 @@
+#[must_use]
+pub fn default<T: Default>() -> T {
+    T::default()
+}
+
+pub fn default_with<T: Default>(f: impl FnOnce(&mut T)) -> T {
+    let mut t = T::default();
+    f(&mut t);
+    t
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,14 @@
 #![deny(unsafe_code)]
 #![deny(clippy::all, clippy::pedantic, clippy::cargo)]
+// ---
+#![cfg_attr(not(any(test, feature = "std")), no_std)]
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+#[macro_use]
+mod macros;
+
+pub mod default;
+pub mod ptr;
