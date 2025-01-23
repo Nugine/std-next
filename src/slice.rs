2c9ff0e9e6781d43dbd207a88bfefc24a4a4f1ba
Nugine <nugine@foxmail.com>
2025-01-23 11:12:26 +0800

    feat(slice): as_chunks_mut_

--- src/slice.rs
+++ src/slice.rs
@@ -4,6 +4,8 @@ use crate::sealed::Sealed;
 
 pub trait SliceExt<T>: Sealed {
     fn as_chunks_<const N: usize>(&self) -> (&[[T; N]], &[T]);
+
+    fn as_chunks_mut_<const N: usize>(&mut self) -> (&mut [[T; N]], &mut [T]);
 }
 
 impl<T> Sealed for [T] {}
@@ -18,10 +20,24 @@ impl<T> SliceExt<T> for [T] {
         let rest = unsafe { slice(base.add(div * N), rem) };
         (chunks, rest)
     }
+
+    fn as_chunks_mut_<const N: usize>(&mut self) -> (&mut [[T; N]], &mut [T]) {
+        assert!(N > 0, "chunk size must be non-zero");
+        let base = self.as_mut_ptr();
+        let len = self.len();
+        let (div, rem) = (len / N, len % N);
+        let chunks = unsafe { slice_mut(base.cast(), div) };
+        let rest = unsafe { slice_mut(base.add(div * N), rem) };
+        (chunks, rest)
+    }
 }
 
 unsafe fn slice<'a, T>(data: *const T, len: usize) -> &'a [T] {
     core::slice::from_raw_parts(data, len)
+}
+
+unsafe fn slice_mut<'a, T>(data: *mut T, len: usize) -> &'a mut [T] {
+    core::slice::from_raw_parts_mut(data, len)
 }
 
 #[cfg(test)]
@@ -30,7 +46,7 @@ mod tests {
 
     #[test]
     fn test_as_chunks() {
-        let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let mut data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
         let (chunks, rest) = data.as_chunks_::<3>();
         assert_eq!(chunks, [[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
         assert_eq!(rest, [10]);
@@ -40,6 +56,18 @@ mod tests {
         assert_eq!(rest, [9, 10]);
 
         let (chunks, rest) = data.as_chunks_::<10>();
+        assert_eq!(chunks, [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]);
+        assert_eq!(rest, &[]);
+
+        let (chunks, rest) = data.as_chunks_mut_::<3>();
+        assert_eq!(chunks, [[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+        assert_eq!(rest, [10]);
+
+        let (chunks, rest) = data.as_chunks_mut_::<4>();
+        assert_eq!(chunks, [[1, 2, 3, 4], [5, 6, 7, 8]]);
+        assert_eq!(rest, [9, 10]);
+
+        let (chunks, rest) = data.as_chunks_mut_::<10>();
         assert_eq!(chunks, [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]);
         assert_eq!(rest, &[]);
     }
