f34d53ed6800872bd052afd45100d6d1a2b1f026
Nugine <nugine@foxmail.com>
2025-03-22 13:45:02 +0800

    feat(iter): add shortcut functions

--- src/iter.rs
+++ src/iter.rs
@@ -4,6 +4,9 @@ use core::fmt;
 #[cfg(feature = "alloc")]
 use alloc::string::String;
 
+#[cfg(feature = "alloc")]
+use alloc::vec::Vec;
+
 pub trait IteratorExt: Iterator {
     #[cfg_attr(docsrs, doc(cfg(feature = "alloc")))]
     #[cfg(feature = "alloc")]
@@ -30,3 +33,41 @@ pub trait IteratorExt: Iterator {
 }
 
 impl<I: Iterator> IteratorExt for I {}
+
+pub fn map_collect<C, T, I, F>(iterable: I, f: F) -> C
+where
+    I: IntoIterator,
+    F: FnMut(I::Item) -> T,
+    C: FromIterator<T>,
+{
+    iterable.into_iter().map(f).collect()
+}
+
+#[cfg_attr(docsrs, doc(cfg(feature = "alloc")))]
+#[cfg(feature = "alloc")]
+pub fn map_collect_vec<T, I, F>(iterable: I, f: F) -> Vec<T>
+where
+    I: IntoIterator,
+    F: FnMut(I::Item) -> T,
+{
+    map_collect(iterable, f)
+}
+
+pub fn filter_map_collect<C, T, I, F>(iterable: I, f: F) -> C
+where
+    I: IntoIterator,
+    F: FnMut(I::Item) -> Option<T>,
+    C: FromIterator<T>,
+{
+    iterable.into_iter().filter_map(f).collect()
+}
+
+#[cfg_attr(docsrs, doc(cfg(feature = "alloc")))]
+#[cfg(feature = "alloc")]
+pub fn filter_map_collect_vec<T, I, F>(iterable: I, f: F) -> Vec<T>
+where
+    I: IntoIterator,
+    F: FnMut(I::Item) -> Option<T>,
+{
+    filter_map_collect(iterable, f)
+}
